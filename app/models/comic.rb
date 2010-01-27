f044831a4194e24f252dfa02f1aa8aa7c94b7dca
Michael Orr <michael@cloudspace.com>
2010-01-26 22:54:10 -0500

    requiring fields for comic and issue model

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,3 +1,4 @@
 class Comic < ActiveRecord::Base
   has_many :issues
+  validates_presence_of :title
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
 class Issue < ActiveRecord::Base
  has_one :comic
+ validates_presence_of :issue_num, :comic_id
 end
