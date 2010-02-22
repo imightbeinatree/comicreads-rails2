22330ad1852a992786ff8ebe2b268b5df1481cb6
Michael Orr <michael@cloudspace.com>
2010-02-22 17:32:15 -0500

    the date is now prefilled with today on the read entry form, remove some debugging statements, minor changes to code spacing

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,16 +1,17 @@
 class ReadsController < ApplicationController
   def new
+    unless @read
+      @read = Read.new
+      @read.read_date = Time.now.strftime("%Y-%m-%d")
+    end
   end
 
   def create
      @read = Read.new()
      @comic, @issue = @read.find_or_create_issue(params[:comic], params[:issue_num]) 
-puts @comic.inspect
-puts @issue.inspect
      @read.issue = @issue
-     @read.read_date = params[:read_date][:read_date]
+     @read.read_date = params[:read][:read_date]
      @read.notes = params[:notes]
-puts @read.inspect
      if @read.save
        flash[:notice] = "Read Saved!"
        redirect_to reads_path
@@ -25,11 +26,12 @@ puts @read.inspect
   end
 
   def destroy
-   @read = Read.find params[:id]
-   @read.destroy
-   flash[:notice] = "read deleted"
-   redirect_to reads_path
+    @read = Read.find params[:id]
+    @read.destroy
+    flash[:notice] = "read deleted"
+    redirect_to reads_path
   end
 
 
 end
+
