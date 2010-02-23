ffab1591a1998c5e07baa6b93867290fbfbb2d25
Michael Orr <michael@cloudspace.com>
2010-02-23 00:02:04 -0500

    now you can enter a coninuous range of comics in a single read and it will expand it out to multiple individual issue reads

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function hide_range_end(){
+  //alert('hide_range_end');
+  //$('range_end').setStyle({display: none});
+  document.getElementById('range_end').style.display = 'none';
+}
+
+function show_range_end(){
+  //alert('show_range_end');
+  //$('range_end').setStyle({display: block});
+  document.getElementById('range_end').style.display = 'block';
+}
